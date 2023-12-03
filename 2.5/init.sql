ab27b22755265dbb7af8176142b89a3c16ab1c01
Nigrim <151794604+Nigrim@users.noreply.github.com>
2023-12-03 16:39:48 +0300

    Update init.sql

--- 2.5/init.sql
+++ 2.5/init.sql
@@ -1,36 +1,58 @@
--- Создание временной таблицы с данными о продажах и планах
-WITH SalesData AS (
-    -- Выбираем необходимые атрибуты и объединяем данные о продажах из разных магазинов
+-- Создание таблицы "shops" с информацией о магазинах
+CREATE TABLE shops (
+    shop_id INT PRIMARY KEY,
+    shop_name VARCHAR(255) NOT NULL
+);
+
+-- Обновление таблицы "plan" с учетом магазинов
+ALTER TABLE plan
+ADD COLUMN shop_id INT,
+ADD FOREIGN KEY (shop_id) REFERENCES shops(shop_id);
+
+-- Создание временной таблицы с объединенными данными из shop_*
+WITH ShopSales AS (
     SELECT
-        pl.shop_name,
-        p.product_name,
-        COALESCE(sd.sales_cnt + mv.sales_cnt + sl.sales_cnt, 0) AS sales_fact, -- Количество фактических продаж
-        pl.plan_cnt AS sales_plan, -- Количество запланированных продаж
-        COALESCE(sd.sales_cnt + mv.sales_cnt + sl.sales_cnt, 0) / pl.plan_cnt AS sales_fact_plan_ratio, -- Отношение фактических продаж к запланированным
-        COALESCE(sd.sales_cnt, 0) * p.price AS income_fact, -- Фактический доход
-        pl.plan_cnt * p.price AS income_plan, -- Планируемый доход
-        COALESCE(sd.sales_cnt, 0) * p.price / NULLIF(pl.plan_cnt * p.price, 0) AS income_fact_plan_ratio -- Отношение фактического дохода к запланированному
+        'DNS' AS shop_name,
+        product_id,
+        sales_cnt
     FROM
-        products p
-    JOIN
-        plan pl ON p.product_id = pl.product_id
-    LEFT JOIN
-        shop_dns sd ON p.product_id = sd.product_id
-    LEFT JOIN
-        shop_mvideo mv ON p.product_id = mv.product_id
-    LEFT JOIN
-        shop_sitilink sl ON p.product_id = sl.product_id
+        shop_dns
+
+    UNION ALL
+
+    SELECT
+        'MVideo' AS shop_name,
+        product_id,
+        sales_cnt
+    FROM
+        shop_mvideo
+
+    UNION ALL
+
+    SELECT
+        'SitiLink' AS shop_name,
+        product_id,
+        sales_cnt
+    FROM
+        shop_sitilink
 )
 
--- Создание итоговой таблицы с объединением
+-- Объединение данных из ShopSales с plan, products и shops
 SELECT
     s.shop_name,
-    s.product_name,
-    s.sales_fact,
-    s.sales_plan,
-    s.sales_fact_plan_ratio,
-    s.income_fact,
-    s.income_plan,
-    s.income_fact_plan_ratio
+    p.product_name,
+    COALESCE(ss.sales_cnt, 0) AS sales_fact,
+    pl.plan_cnt AS sales_plan,
+    COALESCE(ss.sales_cnt, 0) / pl.plan_cnt AS sales_fact_plan_ratio,
+    COALESCE(ss.sales_cnt, 0) * p.price AS income_fact,
+    pl.plan_cnt * p.price AS income_plan,
+    COALESCE(ss.sales_cnt, 0) * p.price / NULLIF(pl.plan_cnt * p.price, 0) AS income_fact_plan_ratio
 FROM
-    SalesData s;
+    ShopSales ss
+JOIN
+    plan pl ON ss.product_id = pl.product_id
+JOIN
+    products p ON ss.product_id = p.product_id
+JOIN
+    shops s ON s.shop_name = ss.shop_name;
+
