c3523a2e27907ec5a091ccdf1a72e0a03b92d50e
Nigrim <151794604+Nigrim@users.noreply.github.com>
2023-12-07 15:22:21 +0300

    Update 3.sql

--- PA1/homework-2-att-1/3.sql
+++ PA1/homework-2-att-1/3.sql
@@ -1,4 +1,49 @@
--- Выбираем данные о поездках, которые завершились на каждой станции за каждый год
+#Создаем таблицу для каждого года
+CREATE TABLE IF NOT EXISTS yearly_summary AS
+SELECT
+    "year",
+    start_station_id AS station_id,
+    COUNT(DISTINCT trip_id) AS trips_started,
+    0 AS trips_ended, -- Инициализируем нулевым значением для последующего обновления
+    COUNT(DISTINCT trip_id) AS trips_total,
+    AVG(duration_minutes) AS avg_dur_started,
+    0 AS avg_dur_ended -- Инициализируем нулевым значением для последующего обновления
+FROM
+    trips
+WHERE
+    "year" BETWEEN 2013 AND 2017
+GROUP BY
+    "year", start_station_id;
+
+#Обновляем таблицу для учета завершенных поездок
+UPDATE
+    yearly_summary AS ys
+SET
+    trips_ended = te.trips_ended,
+    trips_total = ys.trips_started + te.trips_ended,
+    avg_dur_ended = te.avg_dur_ended
+FROM (
+    SELECT
+        "year",
+        end_station_id AS station_id,
+        COUNT(DISTINCT trip_id) AS trips_ended,
+        AVG(duration_minutes) AS avg_dur_ended
+    FROM
+        trips
+    WHERE
+        "year" BETWEEN 2013 AND 2017
+    GROUP BY
+        "year", end_station_id
+) AS te
+WHERE
+    ys."year" = te."year"
+    AND ys.station_id = te.station_id;
+
+#Выводим результат
+SELECT * FROM yearly_summary;
+
+
+#Выбираем данные о поездках, которые завершились на каждой станции за каждый год
 WITH trips_ended AS (
   SELECT
     tr."year",
@@ -10,7 +55,7 @@ WITH trips_ended AS (
   GROUP BY tr."year", tr.end_station_id
 ),
 
--- Выбираем данные о поездках, которые начались на каждой станции за каждый год
+#Выбираем данные о поездках, которые начались на каждой станции за каждый год
 trips_started AS (
   SELECT
     tr."year",
@@ -22,7 +67,7 @@ trips_started AS (
   GROUP BY tr."year", tr.start_station_id
 ),
 
--- Выбираем данные о поездках, которые начались и завершились на одной и той же станции за каждый год
+#Выбираем данные о поездках, которые начались и завершились на одной и той же станции за каждый год
 round_trips AS (
   SELECT
     tr."year",
@@ -33,7 +78,7 @@ round_trips AS (
   GROUP BY tr."year", tr.start_station_id
 )
 
--- Объединяем результаты предыдущих запросов для формирования окончательного результата
+#Объединяем результаты предыдущих запросов для формирования окончательного результата
 SELECT
   te."year",
   te.end_station_id,
